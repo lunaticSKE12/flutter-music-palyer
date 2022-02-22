ee428a2fc89313d2759dc91a5e78d91b38dd34ba
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-02-22 13:20:10 +0700

    Layout and cover

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,64 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Container()),
+      backgroundColor: Colors.grey.shade900,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(24),
+          child: Column(
+            children: [
+              Text(
+                'Now Playing',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Center(
+                child: Container(
+                  height: 280,
+                  width: 280,
+                  margin: EdgeInsets.only(top: 40),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                      image: AssetImage('assets/cover.jpg'),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 40, bottom: 8),
+                child: Column(
+                  children: [
+                    Text(
+                      'The Jam',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    Text(
+                      'Slynk & Mr Stabalina',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 10,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
